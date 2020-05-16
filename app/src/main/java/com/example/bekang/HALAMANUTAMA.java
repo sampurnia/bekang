df7294b34608fa3e19ccf4c9bd5101e5ae7b4e9e
sampurnia <psppuput25@gmail.com>
2020-05-16 12:22:15 +0700

    memperbaiki user location, memperbaiki force stop aplikasi, menghapus halaman mapbox dan menjadikannya halaman utama, menambah dependensi dan repository mapbox, menambahkan string user location permission

--- app/src/main/java/com/example/bekang/HALAMANUTAMA.java
+++ app/src/main/java/com/example/bekang/HALAMANUTAMA.java
@@ -6,22 +6,141 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.mapbox.android.core.permissions.PermissionsListener;
+import com.mapbox.android.core.permissions.PermissionsManager;
+import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.location.LocationComponent;
+import com.mapbox.mapboxsdk.location.LocationComponentActivationOptions;
+import com.mapbox.mapboxsdk.location.modes.CameraMode;
+import com.mapbox.mapboxsdk.location.modes.RenderMode;
+import com.mapbox.mapboxsdk.maps.MapView;
+import com.mapbox.mapboxsdk.maps.MapboxMap;
+import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
+import com.mapbox.mapboxsdk.maps.Style;
 
-public class HALAMANUTAMA extends AppCompatActivity {
+import java.util.List;
+
+public class HALAMANUTAMA extends AppCompatActivity implements PermissionsListener {
 
     private FirebaseAuth firebaseAuth;
-
+    private MapView mapView;
+    private MapboxMap mapboxMap;
+    private PermissionsManager permissionsManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Mapbox.getInstance(this, "pk.eyJ1Ijoic2FtcHVybmlhIiwiYSI6ImNrYTZjcGFpZjA2dzUzMG13NGNxdHVldGEifQ.hJkKxmZcvSZisv4N4PvHLg");
         setContentView(R.layout.activity_halamanutama);
         firebaseAuth = FirebaseAuth.getInstance();
+        Mapbox.getInstance(this, getString(R.string.access_token));
+        mapView = findViewById(R.id.mapView);
+        mapView.onCreate(savedInstanceState);
+        mapView.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(@NonNull MapboxMap mapboxMap) {
+                HALAMANUTAMA.this.mapboxMap = mapboxMap;
+
+                mapboxMap.setStyle(Style.MAPBOX_STREETS, new Style.OnStyleLoaded() {
+                            @Override
+                            public void onStyleLoaded(@NonNull Style style) {
+                                enableLocationComponent(HALAMANUTAMA.this, style);
+                            }
+                        });
+            }
+        });
 
     }
+
+
+    @SuppressWarnings( {"MissingPermission"})
+    private static void enableLocationComponent(HALAMANUTAMA mainActivity, @NonNull Style loadedMapStyle) {
+// Check if permissions are enabled and if not request
+        if (PermissionsManager.areLocationPermissionsGranted(mainActivity)) {
+// Get an instance of the component
+            LocationComponent locationComponent = mainActivity.mapboxMap.getLocationComponent();
+// Activate with options
+            locationComponent.activateLocationComponent(
+                    LocationComponentActivationOptions.builder(mainActivity, loadedMapStyle).build());
+// Enable to make component visible
+            locationComponent.setLocationComponentEnabled(true);
+// Set the component's camera mode
+            locationComponent.setCameraMode(CameraMode.TRACKING);
+// Set the component's render mode
+            locationComponent.setRenderMode(RenderMode.COMPASS);
+        } else {
+            mainActivity.permissionsManager = new PermissionsManager(mainActivity);
+            mainActivity.permissionsManager.requestLocationPermissions(mainActivity);
+        }
+    }
+
+    @Override
+    public void onExplanationNeeded(List<String> permissionsToExplain) {
+        Toast.makeText(this, R.string.user_location_permission_explanation, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onPermissionResult(boolean granted) {
+        if (granted) {
+            mapboxMap.getStyle(new Style.OnStyleLoaded() {
+                @Override
+                public void onStyleLoaded(@NonNull Style style) {
+                    enableLocationComponent(HALAMANUTAMA.this, style);
+                }
+            });
+        } else {
+            Toast.makeText(this, R.string.user_location_permission_explanation, Toast.LENGTH_LONG).show();
+            finish();
+        }
+    }
+
+    @Override
+    @SuppressWarnings( {"MissingPermission"})
+    protected void onStart() {
+        super.onStart();
+        mapView.onStart();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        mapView.onStop();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mapView.onSaveInstanceState(outState);
+    }
+
 
     private void Logout() {
         firebaseAuth.signOut();
@@ -47,11 +166,6 @@ public class HALAMANUTAMA extends AppCompatActivity {
         finish();
     }
 
-    private void Navigation() {
-        Intent intent = new Intent(HALAMANUTAMA.this, MAPBOX.class);
-        startActivity(intent);
-        finish();
-    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -73,9 +187,6 @@ public class HALAMANUTAMA extends AppCompatActivity {
                 break;
             case R.id.InfoMenu:
                 Info();
-                break;
-            case R.id.NavigationMenu:
-                Navigation();
                 break;
         }
         return super.onOptionsItemSelected(item);
