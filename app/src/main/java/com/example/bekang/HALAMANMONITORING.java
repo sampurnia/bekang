6f91c9f72050d78bd674038c46f939bdcc0546e0
Sampurnia <sampurnia@gmail.com>
2020-06-06 10:29:38 +0700

    menghapus recyclerview menjadi activity(class)

--- app/src/main/java/com/example/bekang/HALAMANMONITORING.java
+++ app/src/main/java/com/example/bekang/HALAMANMONITORING.java
@@ -9,9 +9,15 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.bekang.firebaseauth.HALAMANLOGIN;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 /*
 TOLONG DONG INI DIJADIIN FRAGMENT TIAP TRUCK
@@ -29,12 +35,18 @@ public class HALAMANMONITORING extends AppCompatActivity {
     AnimationDrawable animationDrawable1;
     AnimationDrawable animationDrawable2;
     private FirebaseAuth firebaseAuth;
+    TextView a,b,c;
+    DatabaseReference reff;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_halamanmonitoring);
         firebaseAuth = FirebaseAuth.getInstance();
+        a=(TextView)findViewById(R.id.tvWater);
+        b=(TextView)findViewById(R.id.tvTemp);
+        c=(TextView)findViewById(R.id.tvFuel);
+
 
         imageView=(ImageView) findViewById(R.id.imageView);
         imageView.setBackgroundResource(R.drawable.battery);
@@ -63,10 +75,25 @@ public class HALAMANMONITORING extends AppCompatActivity {
     }
 
     private void Refresh() {
-        Intent intent = new Intent(HALAMANMONITORING.this, HALAMANMONITORING.class);
-        startActivity(intent);
-        finish();
+        reff= FirebaseDatabase.getInstance().getReference().child("mobil1").child("sensor");
+        reff.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                String fuel = dataSnapshot.child("fuel").getValue().toString();
+                String temp = dataSnapshot.child("temp").getValue().toString();
+                String water = dataSnapshot.child("water").getValue().toString();
+                a.setText(fuel);
+                b.setText(temp);
+                c.setText(water);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
+
 
     private void List() {
         Intent intent = new Intent(HALAMANMONITORING.this, HALAMANSTATUS.class);
